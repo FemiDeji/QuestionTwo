65ccfc2961362415b839124b7b1e45783496ae5d
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 14:52:13 -0800

    Initial commit

---
+++ src/QuestionTwo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class QuestionTwo {
+    public static void main(String[] args) {
+        int var = 1;
+        int sum = 0;
+        Scanner scanner = new Scanner (System.in);
+        for (i = 1; i < 11; i++) {
+            sum = sum + 1;
+        }
+        System.out.println(sum);
+
+
+    }
+}
